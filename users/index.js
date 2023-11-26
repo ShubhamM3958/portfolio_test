de1cd834ddec3f399715347b42c89524729bf728
Shubham Malhotra <shubhammalhotraa3958@gmail.com>
2023-11-26 19:51:56 +0530

    Create index.js

---
+++ users/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000');
+  //var start = (process.platform == 'darwin' ? 'open' : process.platform == 'win32' ? 'start' : 'xdg-open');
+  //require('child_process').exec(start + ' ' + 'http://localhost:3000');
+})
